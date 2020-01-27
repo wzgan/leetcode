c9a0d3dbb5e457b8481fc8b3dc5b4e6d5f2f9577
wzgan <ganweizhe@gmail.com>
2020-01-27 19:52:36 +0800

    update

--- problems/array/Remove_Duplicates_from_Sorted_Array.py
+++ problems/array/Remove_Duplicates_from_Sorted_Array.py
@@ -5,10 +5,10 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         i = 0
         while i < len(nums)-1:
-            i += 1
-            if(nums[i] == nums[i-1]):
-                del nums[i]
-                i -= 1
+            if(nums[i] == nums[i+1]):
+                del nums[i+1]
+            else:
+                i += 1
         return len(nums)
 
 

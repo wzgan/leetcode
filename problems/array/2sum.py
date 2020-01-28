96e2e69a11eb18e6894f365c03a52ef82918cb0b
wzgan <ganweizhe@gmail.com>
2020-01-28 13:18:55 +0800

    #

--- problems/array/2sum.py
+++ problems/array/2sum.py
@@ -1,7 +1,14 @@
 from typing import List
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twosum_0(self, nums: List[int], target: int) -> List[int]:  # O(n^2)
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:    # O(n)
         hash = {}
         for x in range(len(nums)):
             if target-nums[x] in hash:

6717fbf3f6c56849278cc92dadf9ffa321de274f
甘唯哲 <ganweizhe@192.168.0.102>
2020-07-09 15:33:24 +0800

    f

---
+++ problems/tree/trim_a_binary_search_tree.cpp
@@ -0,0 +1,28 @@
+//
+// Created by 甘唯哲 on 2020/7/9.
+//
+
+// https://leetcode.com/problems/trim-a-binary-search-tree/
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int L, int R)
+    {
+        if (!root) return root;
+        if (root->val < L) return trimBST(root->right, L, R);
+        if (root->val > R) return trimBST(root->left, L, R);
+        root->left = trimBST(root->left, L, R);
+        root->right = trimBST(root->right, L, R);
+        return root;
+    }
+};

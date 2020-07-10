a928edfcc4a4190ca25cadea98495c1a27b65d43
甘唯哲 <ganweizhe@192.168.0.102>
2020-07-10 20:17:10 +0800

    s

---
+++ problems/tree/lowest_common_ancestor_of_a_binary_search_tree.cpp
@@ -0,0 +1,32 @@
+//
+// Created by 甘唯哲 on 2020/7/10.
+//
+
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        if (!root) return nullptr;
+
+        //if (root == p || root == q) return root;
+
+        if (p->val < root->val && q->val < root->val)
+            return lowestCommonAncestor(root->left, p, q);
+        else if (p->val > root->val && q->val > root->val)
+            return lowestCommonAncestor(root->right, p, q);
+        else
+            return root;
+    }
+};

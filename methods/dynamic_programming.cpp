765ac0f9e6fd22a1a5730d7eec0996b582859365
甘唯哲 <ganweizhe@192.168.0.102>
2020-07-14 18:19:25 +0800

    s

--- methods/dynamic_programming.cpp
+++ methods/dynamic_programming.cpp
@@ -108,6 +108,28 @@ int coinChange3(std::vector<int>& coins, int amount)
     return dp[amount] == amount + 1 ? -1 : dp[amount];
 }
 
+//按照完全背包问题的递推关系式
+int coinsChange4(std::vector<int>& coins, int amount)
+{
+    std::vector<std::vector<int>> dp(coins.size()+1, std::vector<int>(amount+1, amount+1));
+    for (int i = 0; i <= coins.size() ; ++i)
+    {
+        dp[i][0] = 0;
+    }
+    for (int j = 1; j <= amount; ++j)
+    {
+        dp[0][j] = amount + 1;
+    }
+    for (int i = 1; i <= coins.size() ; ++i)
+    {
+        for (int j = 1; j <= amount; ++j)
+        {
+            if (j < coins[i-1]) dp[i][j] = dp[i-1][j];
+            else dp[i][j] = std::min(dp[i-1][j], 1 + dp[i][j-coins[j-1]]);
+        }
+    }
+    return dp[coins.size()][amount] == amount + 1 ? -1 : dp[coins.size()][amount];
+}
 
 //
 //背包问题

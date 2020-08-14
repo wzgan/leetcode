bc15ad6f4b11a0820d4401951580edaf9d12ff03
甘唯哲 <ganweizhe@192.168.0.103>
2020-08-15 00:59:28 +0800

    s

--- problems/linked_list/linked_list_cycle_ii.cpp
+++ problems/linked_list/linked_list_cycle_ii.cpp
@@ -2,3 +2,42 @@
 // Created by 甘唯哲 on 2020/7/30.
 //
 
+//https://leetcode.com/problems/linked-list-cycle-ii/
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head)
+    {
+        if (!head || !head->next)
+            return nullptr;
+
+        ListNode* slow = head, *quick = head;
+        while(slow && quick && quick->next)
+        {
+            slow = slow->next;
+            quick = quick->next->next;
+
+            if (slow == quick)
+                break;
+        }
+
+        if (slow != quick)
+            return nullptr;
+
+        slow = head;
+        while (slow != quick)
+        {
+            slow = slow->next;
+            quick = quick->next;
+        }
+        return slow;
+    }
+};
--- problems/linked_list/merge_k_sorted_lists.cpp
+++ problems/linked_list/merge_k_sorted_lists.cpp
@@ -2,3 +2,57 @@
 // Created by 甘唯哲 on 2020/7/28.
 //
 
+#include <vector>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* merge(std::vector<ListNode*>& lists, int start, int end)
+    {
+        if (start == end) return lists[start];
+        int mid = (start + end) / 2;
+        ListNode* node1 = merge(lists, start, mid);
+        ListNode* node2 = merge(lists, mid+1, end);
+
+        ListNode* p1 = node1, *p2 = node2, *p0 = new ListNode, *p = p0;
+        while (p1 != nullptr && p2 != nullptr)
+        {
+            if (p1->val <= p2->val)
+            {
+                p->next = p1;
+                p = p->next;
+                p1 = p1->next;
+            } else
+            {
+                p->next = p2;
+                p = p->next;
+                p2 = p2->next;
+            }
+        }
+        if (p1 != nullptr)
+        {
+            p->next = p1;
+            p = p->next;
+        }
+        if (p2 != nullptr)
+        {
+            p->next = p2;
+            p = p->next;
+        }
+        return p0->next;
+    }
+
+    ListNode* mergeKLists(std::vector<ListNode*>& lists)
+    {
+        if (lists.empty()) return nullptr;
+        int start = 0, end = lists.size() - 1;
+        return merge(lists, start, end);
+    }
+};

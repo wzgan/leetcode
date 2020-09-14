97f5bc4ee3cbf669683137733c1187c9054f8a3b
wzgan <ganweizhe@gmail.com>
2020-09-14 18:21:46 +0800

    s

--- problems/stack/daily-temperatures.cpp
+++ problems/stack/daily-temperatures.cpp
@@ -12,15 +12,15 @@ public:
     std::vector<int> dailyTemperatures(std::vector<int>& T)
     {
         std::vector<int> res(T.size(), 0);
-        std::stack<std::pair<int, int>> s;
+        std::stack<int> s;
         for (int i = T.size()-1; i >= 0; --i)
         {
-            while (!s.empty() && s.top().first <= T[i])
+            while (!s.empty() && T[s.top()] <= T[i])
             {
                 s.pop();
             }
-            res[i] = s.empty() ? 0 : s.top().second-i;
-            s.push(std::make_pair(T[i], i));
+            res[i] = s.empty() ? 0 : s.top()-i;
+            s.push(i);
         }
         return res;
     }

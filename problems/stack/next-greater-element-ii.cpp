2327567396aed56ad614d2c808df8ba8676da585
wzgan <ganweizhe@gmail.com>
2020-09-14 18:26:30 +0800

    s

--- problems/stack/next-greater-element-ii.cpp
+++ problems/stack/next-greater-element-ii.cpp
@@ -11,26 +11,27 @@ class Solution {
 public:
     std::vector<int> nextGreaterElements(std::vector<int>& nums)
     {
-        std::vector<int> nums_new(nums.size()*2, 0);
-        for (int i = 0; i < nums.size(); ++i)
-        {
-            nums_new[i] = nums[i];
-            nums_new[i+nums.size()] = nums[i];
-        }
+//        std::vector<int> nums_new(nums.size()*2, 0);
+//        for (int i = 0; i < nums.size(); ++i)
+//        {
+//            nums_new[i] = nums[i];
+//            nums_new[i+nums.size()] = nums[i];
+//        }
 
         std::vector<int> res(nums.size(), -1);
         std::stack<int> s;
-        for (int j = nums_new.size()-1; j >= 0; --j)
+        for (int j = nums.size()*2-1; j >= 0; --j)
         {
-            while (!s.empty() && s.top() <= nums_new[j])
+            while (!s.empty() && s.top() <= nums[j%nums.size()])
             {
                 s.pop();
             }
-            if (j < nums.size())
-            {
-                res[j] = s.empty() ? -1 : s.top();
-            }
-            s.push(nums_new[j]);
+//            if (j < nums.size())
+//            {
+//                res[j] = s.empty() ? -1 : s.top();
+//            }
+            res[j%nums.size()] = s.empty() ? -1 : s.top();
+            s.push(nums[j%nums.size()]);
         }
         return res;
     }

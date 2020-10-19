cf90074b435ed787be0f4b5c196f9bffe1e3c976
wzgan <ganweizhe@gmail.com>
2020-10-19 18:05:27 +0800

    ss

--- problems/tree/populating-next-right-pointers-in-each-node-ii.cpp
+++ problems/tree/populating-next-right-pointers-in-each-node-ii.cpp
@@ -16,10 +16,21 @@ public:
         : val(_val), left(_left), right(_right), next(_next) {}
 };
 
+#include <queue>
+
 class Solution {
 public:
+	//bfs level-traverse
     Node* connect(Node* root) 
     {
-        
+        std::queue<Node*> q1;
+        std::queue<Node*> q2;
+
+       
+        while(!q1.empty() && !q2.empty())
+        {
+
+        }
+
     }
 };

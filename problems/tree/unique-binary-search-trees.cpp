7b745412d4714ae09c1984d34015dfb6403fae36
wzgan <ganweizhe@gmail.com>
2020-10-29 19:34:50 +0800

    s

---
+++ problems/tree/unique-binary-search-trees.cpp
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/unique-binary-search-trees/
+
+#include <vector>
+
+class Solution {
+public:
+    int numTrees(int n) 
+    {
+        std::vector<int> memo(n+1, 0);
+        memo[1] = 1;
+        for (int i = 2; i <= n; ++i)
+        {
+        	int res = 2*memo[i-1];
+        	for (int j = 1; j <= i-2; ++j)
+        	{
+        		res += memo[j] * memo[n-j-1];
+        	}
+        	memo[i] = res;
+        }
+        return memo[n];
+    }
+};
+

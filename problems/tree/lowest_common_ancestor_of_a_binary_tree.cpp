34da0c15c86746678577ddfbe9671479ad37f805
ganweizhe (RD) <gan.weizhe@h3c.com>
2021-02-10 10:07:47 +0800

    's'

--- problems/tree/lowest_common_ancestor_of_a_binary_tree.cpp
+++ problems/tree/lowest_common_ancestor_of_a_binary_tree.cpp
@@ -17,10 +17,7 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
     {
-        if (!root)
-            return nullptr;
-
-        if (root->val == p->val || root->val == q->val)
+        if (!root || root->val == p->val || root->val == q->val)
             return root;
 
         TreeNode* lRet = lowestCommonAncestor(root->left, p, q);

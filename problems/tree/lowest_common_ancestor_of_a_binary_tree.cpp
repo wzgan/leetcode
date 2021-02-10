6406b9f6687b297f6ac88f68540a643fe9611202
ganweizhe (RD) <gan.weizhe@h3c.com>
2021-02-10 17:47:54 +0800

    's'

--- problems/tree/lowest_common_ancestor_of_a_binary_tree.cpp
+++ problems/tree/lowest_common_ancestor_of_a_binary_tree.cpp
@@ -13,6 +13,11 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
@@ -27,5 +32,76 @@ public:
             return root;
         else
             return rRet ? rRet : lRet;
+    }
+
+    //bfs
+    TreeNode* lowestCommonAncestor2(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        std::queue<TreeNode*> nodeQue;
+        std::unordered_map<TreeNode*, TreeNode*> parentsMap;
+
+        nodeQue.push(root);
+        parentsMap.insert(std::make_pair(root, nullptr));
+        while (!parentsMap.count(p) || !parentsMap.count(q))
+        {
+            TreeNode* curNode = nodeQue.front();
+            nodeQue.pop();
+
+            if (curNode->left)
+            {
+                nodeQue.push(curNode->left);
+                parentsMap.insert(std::make_pair(curNode->left, curNode));
+            }
+
+            if (curNode->right)
+            {
+                nodeQue.push(curNode->right);
+                parentsMap.insert(std::make_pair(curNode->right, curNode));
+            }
+        }
+
+        std::unordered_set<TreeNode*> ancestors;
+        while (p)
+        {
+            ancestors.insert(p);
+            p = parentsMap[p];
+        }
+        while (!ancestors.count(q))
+            q = parentsMap[q];
+        return q;
+    }
+
+    //dfs
+    struct NewNode
+    {
+        TreeNode* node;
+        bool bVisited;
+        TreeNode* lRes;
+        TreeNode* rRes;
+        NewNode* parent;
+
+        NewNode(TreeNode* node) : node(node), bVisited(false), lRes(nullptr), rRes(nullptr), parent(nullptr) {}
+    };
+    TreeNode* lowestCommonAncestor3(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        std::stack<NewNode> nodeStack;
+        nodeStack.push(NewNode(root));
+
+        while (!nodeStack.empty())
+        {
+            NewNode n_node = nodeStack.top();
+
+            if (!n_node.node || n_node.node == p || n_node.node == q)
+            {
+
+            }
+            if (!n_node.bVisited)
+            {
+                nodeStack.push(NewNode(root->right));
+                nodeStack.push(root->left);
+            } else if ()
+
+
+        }
     }
 };
